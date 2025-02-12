55832da9df21e9dd55a0d3392449dbe1e9d4b5ce
maycol-xd <maycolasas18@gmail.com>
2025-02-12 18:46:58 -0500

    Create promedio de tres notas.py

---
+++ promedio de tres notas.py
@@ -0,0 +1,5 @@
+nota1  =  float ( input ( "Ingresa la primera calificación: " ))
+nota2  =  float ( input ( "Ingresa la segunda calificación: " ))
+nota3  =  float ( input ( "Ingresa la tercera calificación: " ))
+promedio  = ( nota1  +  nota2  +  nota3 ) /  3
+print ( "El promedio es:" , round ( promedio , 2 ))
