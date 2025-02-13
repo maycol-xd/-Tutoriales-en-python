5927a6d22508ab083cf211e849d8127e01ed24f2
Michael-punina <maycolasas18@gmail.com>
2025-02-12 22:15:36 -0500

    Update suma de dos numeros.py

--- suma de dos numeros.py
+++ suma de dos numeros.py
@@ -1,4 +1,4 @@
-suma1 =  int ( input ( "Ingresa el primer número: " ))
-suma2 =  int ( input ( "Ingresa el segundo número: " ))
-suma =  num1 + num2
-print ( "La suma es:" ,  suma )
+sum1 =  int ( input ( "Ingresa el primer número: " ))
+sum2 =  int ( input ( "Ingresa el segundo número: " ))
+sum =  sum1 + sum2
+print ( "La suma es:" ,  sum)
